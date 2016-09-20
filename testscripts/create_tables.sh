cb84d43f6b2a742aa5eae0e4c92e19e63a89c4e3
Peter Vandenberghe <petervanden@gmail.com>
2016-09-20 08:26:33 +0200

    Added test scripts

---
+++ testscripts/create_tables.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sqlite3 coc_utils.sqlite < create_tables.sql
---
+++ testscripts/create_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE TblClans(
+  ClTag text PRIMARY KEY,
+  ClName text NOT NULL,
+  ClDescription text,
+  ClType text,
+  ClLocation text,
+  ClBadgeURL text,
+  ClWarFrequency text,
+  ClClanLevel int,
+  ClWarWins int,
+  ClWarWinStreak int,
+  ClClanPoints int,
+  ClRequiredTrophies int,
+  ClMembers int,
+  ClCreated text,
+  ClUpdated text
+
+);
+CREATE TABLE TblMembers(
+  MbTag text PRIMARY KEY,
+  MbName text NOT NULL,
+  MbClTag text,
+  MbRole text,
+  MbLevel int,
+  MbLeagueId int,
+  MbLeague text,
+  MbTrophies int,
+  MbClanRank int,
+  MbDonations int,
+  MbReceived int,
+  MbCreated text,
+  MbUpdated text
+);
